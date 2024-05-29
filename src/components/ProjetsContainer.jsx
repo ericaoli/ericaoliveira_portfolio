058cb4d2e1cf56315d43431796c0aef8b6d13007
ericaoli <oliveira.emp@yahoo.com>
2024-05-29 11:44:48 +0200

    mise à jour projets

--- src/components/ProjetsContainer.jsx
+++ src/components/ProjetsContainer.jsx
@@ -3,7 +3,7 @@ const ProjetsContainer = () => {
   return (
     <section className="projects-container">
         <h2>Projets</h2>
-        <p>Dans mon profil sur GitHub vous trouverez mes repositoires de code! </p>
+        <p>Dans mon profil sur GitHub vous trouverez ce que je suis en train de coder! </p>
         <a href="https://github.com/ericaoli" className="btn">Voir mes projets</a>
     </section>
   )
